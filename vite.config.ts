094f9b76c64fd8f49cb229952985b6a97f0e83cc
Dean Gilmore <dean@smartcrm.vip>
2026-05-05 23:56:53 +0000

    fix: Remove unused compression plugin import from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { compression } from 'vite-plugin-compression2';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
